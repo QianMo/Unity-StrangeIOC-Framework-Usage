9ee866f465642dc70815cb2de4b24b7d5c883021
浅墨 <happylifemxy@163.com>
2017-11-29 15:48:01 +0800

    add
    add

---
+++ Assets/Framework/Editor/AudioWindowEditor.cs
@@ -0,0 +1,22 @@
+﻿//-------------------------------------------------------------------------------------
+//	AudioWindowEditor.cs
+//
+//	Created by 浅墨
+//-------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AudioWindowEditor : EditorWindow
+{
+
+    [MenuItem("test/test1")]
+    static void CreateWindow()
+    {
+        Rect rect = new Rect(400, 400, 400, 400);
+        AudioWindowEditor window = EditorWindow.GetWindowWithRect(typeof(AudioWindowEditor), rect) as AudioWindowEditor;
+        window.Show();
+    }
+
+}
