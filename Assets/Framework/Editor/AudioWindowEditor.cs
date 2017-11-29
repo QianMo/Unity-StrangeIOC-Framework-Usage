465fe6513bea4c6a7b1b76bab59325c1db399912
浅墨 <happylifemxy@163.com>
2017-11-29 17:57:38 +0800

    add
    add

--- Assets/Framework/Editor/AudioWindowEditor.cs
+++ Assets/Framework/Editor/AudioWindowEditor.cs
@@ -1,5 +1,5 @@
 ﻿//-------------------------------------------------------------------------------------
-//	AudioWindowEditor.cs
+//	test.cs
 //
 //	Created by 浅墨
 //-------------------------------------------------------------------------------------
@@ -7,8 +7,9 @@
 using UnityEngine;
 using System.Collections;
 using UnityEditor;
-using UnityEngine.UI;
 using System.Collections.Generic;
+using System.Text;
+using System.IO;
 
 public class AudioWindowEditor : EditorWindow
 {
@@ -17,8 +18,9 @@ public class AudioWindowEditor : EditorWindow
     static void CreateWindow()
     {
         // method 1
-        //Rect rect = new Rect(400, 400, 400, 400);
-        //AudioWindowEditor window = EditorWindow.GetWindowWithRect(typeof(AudioWindowEditor), rect) as AudioWindowEditor;
+        //             Rect rect = new Rect(400, 400, 400, 400);
+        //             test window2 = EditorWindow.GetWindowWithRect(typeof(test), rect) as test;
+        //             window2.Show();
 
         // method 2
         AudioWindowEditor window = EditorWindow.GetWindow<AudioWindowEditor>("音效管理");
@@ -27,13 +29,51 @@ public class AudioWindowEditor : EditorWindow
 
 
 
+    public string audioName = "Ding";
 
-    public string audioName;
-    public string audioPath;
+    public string audioPath = "Audio/Ding";
+
     private Dictionary<string, string> audioDict = new Dictionary<string, string>();
+
+    public string savePath = "";
+
+
+
+    void Awake()
+    {
+        // savePath = Application.dataPath + "\\Framework\\Resources\\audioList.txt";
+        //LoadAudioList();
+    }
 
     void OnGUI()
     {
+        //return;
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("名称");
+        GUILayout.Label("路径");
+        GUILayout.Label("操作");
+        GUILayout.EndHorizontal();
+
+
+        foreach (string key in audioDict.Keys)
+        {
+            string value;
+            audioDict.TryGetValue(key, out value);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(key);
+            GUILayout.Label(value);
+
+            if (GUILayout.Button("删除"))
+            {
+
+                audioDict.Remove(key);
+                //LoadAudioList();
+                return;
+            }
+            GUILayout.EndHorizontal();
+        }
+
+
         audioName = EditorGUILayout.TextField("音效名称", audioName);
         //GUILayout.TextField("输入文字2");
         audioPath = EditorGUILayout.TextField("音效路径", audioPath);
@@ -54,17 +94,73 @@ public class AudioWindowEditor : EditorWindow
                 }
                 else
                 {
-                    Debug.LogWarning("添加音效"+audioName+"成功！");
+                    Debug.LogWarning("添加音效" + audioName + "成功！");
                     audioDict.Add(audioName, audioPath);
+                    //SaveAudioList();
                 }
-                
+
             }
 
         }
 
+    }
+
+    //------------------------------------------------------------------------
+    // 每秒调用10次
+    //------------------------------------------------------------------------
+    //     void OnInspectorUpdate()
+    //     {
+    //         //SaveAudioList();
+    //     }
+
+    //------------------------------------------------------------------------
+    // 保存List到文件
+    //------------------------------------------------------------------------
+    private void SaveAudioList()
+    {
+        StringBuilder strb = new StringBuilder();
+
+        foreach (string key in audioDict.Keys)
+        {
+            string value;
+            audioDict.TryGetValue(key, out value);
+            strb.Append(key + "," + value + "\n");
+        }
 
 
 
+        File.WriteAllText(savePath, strb.ToString());
+
+        Debug.LogWarning("已经保存到路径:" + savePath);
+    }
+
+
+    //------------------------------------------------------------------------
+    // 从文件读取List
+    //------------------------------------------------------------------------
+    private void LoadAudioList()
+    {
+
+
+        if (File.Exists(savePath) == false)
+        {
+            return;
+        }
+        string[] lines = File.ReadAllLines(savePath);
+        foreach (string line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                continue;
+            }
+
+            string[] keyvalue = line.Split(',');
+            if (keyvalue == null || keyvalue.Length < 2)
+            {
+                continue;
+            }
+            audioDict.Add(keyvalue[0], keyvalue[1]);
+        }
     }
 
 }
