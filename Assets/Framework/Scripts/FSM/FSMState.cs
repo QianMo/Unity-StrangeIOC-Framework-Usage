f305982471e8099e50553bc13f9fc19dbc3a11fe
浅墨 <happylifemxy@163.com>
2017-12-01 00:39:34 +0800

    add
    add

---
+++ Assets/Framework/Scripts/FSM/FSMState.cs
@@ -0,0 +1,26 @@
+﻿//-------------------------------------------------------------------------------------
+//	FSMState.cs
+//
+//	Created by 浅墨
+//-------------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//有哪些状态转换的条件
+public enum Transition
+{
+    NullTransition = 0
+}
+
+public enum StateID
+{
+    NullStateID = 0
+}
+
+public abstract class FSMState
+{
+
+
+}
