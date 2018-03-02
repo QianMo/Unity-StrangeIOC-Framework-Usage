cdaf24d662b231a75e22198eae7d87d9e3ffe98f
浅墨 <happylifemxy@163.com>
2018-03-02 09:24:19 +0800

    优化抽象泛型单例的基类
    优化抽象泛型单例的基类

--- Assets/Framework/Scripts/Utility/Singleton.cs
+++ Assets/Framework/Scripts/Utility/Singleton.cs
@@ -11,16 +11,24 @@ using UnityEngine;
 
 /// <summary>
 /// 单例类基类（抽象类、泛型，其他类只需继承此类即可成为单例类）
-/// 继承该类的，相当于一个单例组件，如UIManager等
+/// 继承该类的，即成为一个单例类
 /// </summary>
-public abstract class Singleton<T> : MonoBehaviour
-    where T : MonoBehaviour
+public abstract class Singleton<T>
+    where T : class, new()
 {
     private static T _instance = null;
 
     public static T Instance
     {
-        get { return _instance; }
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new T();
+                return _instance;
+            }
+            return _instance;
+        }
     }
 
     protected virtual void Awake()
